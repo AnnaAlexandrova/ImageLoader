03aa660ff763d88f9d2df2ef410b3e8e97e95a9c
AnnaAlexandrova <alexandrova-anna@bk.ru>
2020-02-19 16:55:40 +0700

    Fixed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {Header} from './Header/Header';
 import {ImageLoader} from './Main/ImageLoader';
 import {NasaDayPhoto} from './Main/NasaDayPhoto';
 import {Footer} from './Footer/Footer';
 import {NotFoundPage} from './Header/NotFoundPage/NotFoundPage';
-import './App.css';
-import 'normalize.css';
+
 
 export default class App extends React.Component {
     render() {
